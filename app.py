6d853333f1f653d6c9476b255ba0b6ced25f0e44
unknown <qoutioussama2002@gmail.com>
2024-02-23 17:44:42 +0100

    app

---
+++ app.py
@@ -0,0 +1,100 @@
+from openai import OpenAI
+import streamlit as st
+from PIL import Image
+from utils_1 import *
+import base64
+import requests
+import io
+from langchain.prompts import PromptTemplate
+
+
+agent_executor = setup_agent()
+
+template="""You are insurAI an expert in assitant used by top insurence companies your main role is to help the clients in their accident .
+you can answer anyhting else and you answers should always be helpful and provide assitant for the users
+start by doing checking the images the user provided 
+remember to give helpful advices then keep ask follow up questions to the users until you have a clear understanding of the problem they are having 
+you can also estimate how much their insurence will give them back based on the damage of the car and the situation 
+You have access to the following tools:
+\n\nknowledge search: useful for when you need information about questions about insurance. 
+\nweb search: use this tool when you can't find the content in the knowledge base and you need more advanced search functionalities
+\n\nUse the following format:\n\nQuestion: the input question you must answer\nThought: you should always think about what to do\nAction: the action to take, should be one of [knowledge search, web search]\n
+Action Input: the input to the action\nObservation: the result of the action\n... (this Thought/Action/Action Input/Observation can repeat N times)\n
+Thought: I now know the final answer\n
+Final Answer: the final answer to the original input question\n\nBegin!\n\n
+Question: {input}\nThought:{agent_scratchpad}"""
+promptup = PromptTemplate(
+    input_variables=['agent_scratchpad', 'input'],
+    template = template
+)
+
+agent_executor.agent.llm_chain.prompt = promptup
+
+
+api_key = st.secrets["OPENAI_API_KEY"]
+
+def clear_chat_history():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+    uploaded_file = None
+    agent_executor.memory.clear()
+
+# App title
+st.set_page_config(page_title="InsurAI is Here 🦺")
+logo_image_path = "logo.png"
+logo_image = Image.open(logo_image_path)
+resized_logo_image = logo_image.resize((250, 250))  # Adjust the width and height as needed
+st.image(resized_logo_image, use_column_width=True)
+# Replicate Credentials
+with st.sidebar:
+    st.title('Meet InsurAI 🤖🦺')
+    st.markdown(""" 
+Our app transforms the auto insurance journey, simplifying claims and post-accident guidance for enhanced customer satisfaction. As technology reshapes the industry, the app addresses evolving challenges, offering a streamlined solution to modernize the auto insurance experience.
+""")
+    st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+
+
+avatar_img="avatar.png"
+
+# Set OpenAI key
+client = OpenAI(api_key=api_key)
+
+
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+
+# Display or clear chat messages
+for message in st.session_state.messages:
+    if message["role"] == "user" :
+      with st.chat_message(message["role"]):
+          st.write(message["content"])
+    else : 
+      with st.chat_message(message["role"],avatar = avatar_img ):
+          st.write(message["content"])
+
+# User-provided prompt
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
+
+uploaded_file = st.file_uploader("Choose an image file", type="jpg")
+
+# Generate a new response if last message is not from assistant
+if st.session_state.messages[-1]["role"] != "assistant":
+  result = ' '
+  if uploaded_file is not None :
+    image = Image.open(uploaded_file)
+    result = encode_and_query_api(image, api_key)
+  with st.chat_message("assistant", avatar="logo.png"):
+    st.markdown(result)
+    with st.spinner("Thinking..."):
+      response = agent_executor.invoke(result + " " +  st.session_state.messages[-1]["content"])
+      placeholder = st.empty()
+      full_response = ''
+      for item in response['output']:
+        full_response += item
+      placeholder.markdown(full_response)
+    placeholder.markdown(full_response)
+    message = {"role": "assistant", "content": full_response + result}
+    st.session_state.messages.append(message)
+    uploaded_file = None
---
+++ utils_1.py
@@ -0,0 +1,146 @@
+from langchain.embeddings import SentenceTransformerEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import Chroma
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import MessagesPlaceholder
+from langchain.chains import RetrievalQA
+from typing import Tuple, Dict
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts.chat import MessagesPlaceholder
+from langchain.agents import initialize_agent, Tool
+from langchain.agents import AgentExecutor
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.tools.tavily_search import TavilySearchResults
+from dotenv import load_dotenv
+import os
+import io
+import base64
+import requests
+from PIL import Image
+
+load_dotenv()
+
+tavily_api_key = os.getenv("TAVILY_API_KEY")
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+embeddings = OpenAIEmbeddings()
+
+
+
+
+def encode_and_query_api(image, api_key):
+    buffered = io.BytesIO()
+    image.save(buffered, format="JPEG")
+    base64_image = base64.b64encode(buffered.getvalue()).decode("utf-8")
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_key}"
+    }
+
+    payload = {
+        "model": "gpt-4-vision-preview",
+        "messages": [
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": "Please describe this image as good and authentic as possible , include all it details possible as it will be fed to and auto claim system to insurence companies"
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{base64_image}"
+                        }
+                    }
+                ]
+            }
+        ],
+        "max_tokens": 300
+    }
+
+    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+    return response.json()['choices'][0]['message']['content']
+
+
+
+
+
+urls = [
+    "https://en.wikipedia.org/wiki/Vehicle_insurance_in_the_United_States",
+]
+
+docs = [WebBaseLoader(url).load() for url in urls]
+docs_list = [item for sublist in docs for item in sublist]
+
+text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=250, chunk_overlap=0
+)
+doc_splits = text_splitter.split_documents(docs_list)
+
+# Add to vectorDB
+vectorstore = Chroma.from_documents(
+    documents=doc_splits,
+    collection_name="rag-chroma",
+    embedding=embeddings,
+)
+
+class Config():
+    """
+    Contains the configuration of the LLM.
+    """
+    model = 'gpt-3.5-turbo'
+    llm = ChatOpenAI(temperature=0, model=model)
+cfg = Config()
+qa = RetrievalQA.from_chain_type(
+    llm=cfg.llm,
+    chain_type="stuff",
+    retriever = vectorstore.as_retriever()
+)
+
+def setup_memory() -> Tuple[Dict, ConversationBufferMemory]:
+    """
+    Sets up memory for the open ai functions agent.
+    :return a tuple with the agent keyword pairs and the conversation memory.
+    """
+    agent_kwargs = {
+        "extra_prompt_messages": [MessagesPlaceholder(variable_name="memory")],
+    }
+    memory = ConversationBufferMemory(memory_key="memory", return_messages=True)
+    return agent_kwargs, memory
+
+def setup_agent() -> AgentExecutor:
+    """
+    Sets up the tools for a function based chain.
+    We have here the following tools:
+
+    """
+    cfg = Config()
+    tools = [
+        Tool(
+            name="knowledge search",
+            func=qa.run,
+            description="useful for when you need more advanced search option to answer questions about insurence. "
+        ),
+        Tool(
+        name='web search',
+        func=TavilySearchResults(api_key=tavily_api_key).run,
+        description=(
+            '''use this tool when you can't find the content in the knowledge base and you need more advenced search functionalities  '''
+        ))
+        
+    ]
+    agent_kwargs, memory = setup_memory()
+
+    return initialize_agent(
+        tools,
+        cfg.llm,
+        verbose=False,
+        agent_kwargs=agent_kwargs,
+        memory=memory,
+        handle_parsing_errors=True
+    )
+
